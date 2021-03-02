ed78f19f2745e594ec95a0afac0d06e207f1143f
Cannon <73777697+CannonStealth@users.noreply.github.com>
2021-03-02 15:00:28 +0000

    Create types.d.ts

---
+++ source/utils/types.d.ts
@@ -0,0 +1,4 @@
+declare const longformat: (converted: number[]) => string[];
+declare const shortformat: (converted: number[]) => string[];
+export { longformat as long };
+export { shortformat as short };
