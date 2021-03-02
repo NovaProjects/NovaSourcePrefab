996512b39d40720c5e45d4f770968fe29af4345f
Cannon <73777697+CannonStealth@users.noreply.github.com>
2021-03-02 14:59:09 +0000

    Create dates.d.ts

---
+++ source/utils/dates.d.ts
@@ -0,0 +1,4 @@
+declare const convert: (date1: any, decimals?: number, date2?: any, action?: string) => any;
+declare const ms: (array: number[], format?: string) => string;
+export { convert };
+export default ms;
