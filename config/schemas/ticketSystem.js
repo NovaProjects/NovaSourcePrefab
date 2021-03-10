1b4a68aba574a243af3d83353e0ebf159e961d9e
Exxon <tomfagan03@gmail.com>
2021-03-10 20:04:52 +0000

    Added create ticket command and changed schema

--- config/schemas/ticketSystem.js
+++ config/schemas/ticketSystem.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose')
 
 const ticket = new mongoose.Schema({
-  guildId: { type: String },
-  userId: { type: String },
-  channelId: { type: String },
+  gId: { type: String },
+  chanId: { type: String },
   closed: { type: Boolean }, 
   claimed: { type: Boolean },
-  claimId: { type: Boolean } 
+  claimId: { type: Boolean } // i found you
 })
 
 module.exports = mongoose.model('ticket', ticket) 
-// This schema is for tickets .So when you open a ticket this schema will be used, same for when you close / claim a ticket. The other ticket is the ticket config schema for when using the !ticketconfig <Option> command (Not made yet)
