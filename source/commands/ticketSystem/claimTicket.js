6aac1f15c035bf58b53b84d9371afa8c73c428a6
Exxon <tomfagan03@gmail.com>
2021-03-11 16:58:18 +0000

    Added claim ticket command

--- source/commands/ticketSystem/claimTicket.js
+++ source/commands/ticketSystem/claimTicket.js
@@ -1,38 +1,63 @@
 const { PREFIX } = require('../../../config/botconfig.json');
 
-module.exports = { //! This command will be used to creat a new ticket. The ticket guildId, ownerId and channelId will be stored in the database. Claimed and closed will be set to false
+module.exports = {
     name: "claim",
     aliases: ["claimticket", "createticket"],
     description: "Claim a ticket",
     usage: `${PREFIX}claim`, 
     examples: `\`${PREFIX}\`claim`,
     perms: [],
-    cooldown: 10000,
+    cooldown: 15,
     devOnly: false, 
-    execute: async function(client, message, args) {
+    execute: async function(client, message) {
         
         const thisTicket = await client.DBTickets.findOne({ gId: message.guild.id, chanId: message.channel.id })
         if (!thisTicket) return message.channel.send('You can only claim ticket channels!')
 
         const config = await client.DBTicketConfig.findOne({ gId: message.guild.id })
-        const role = message.guild.roles.cache.get(config.roleId)
-        if (!message.member.roles.cache.has(role) && !message.member.permissions.has('ADMINISTRATOR')) return message.channel.send('Only people with the support roles can claim tickets')
 
-        message.channel.updateOverwrite([
+        if (!config) return message.channel.send('put here some text')
+
+        let role;
+
+        let permissions = [
             {
-                id: config.supportRole,
-                deny: 1024
-            }, {
                 id: message.author.id,
                 allow: 117760
             }
-        ], `Claiming ticket for ${message.author.tag}`)
+        ]
+
+        if (config.roleId) {
+
+            role = message.guild.roles.cache.get(config.roleId)
+ 
+            if (!role) await client.DBTicketConfig.findOneAndUpdate({ gId: message.guild.id }, { gId: message.guild.id, roleId: null })
+
+            if (!message.member.roles.cache.has(config.roleId) && !message.member.permissions.has('ADMINISTRATOR')) return message.channel.send('Only people with the support roles can claim tickets')
+
+            permissions.push({
+                id: config.roleId,
+                deny: 1024
+            })
+        }
+
+        message.channel.overwritePermissions(permissions, `Claiming ticket for ${message.author.tag}`)
 
         client.DBTickets.findOneAndUpdate({ gId: message.guild.id, chanId: message.channel.id }, { gId: message.guild.id, claim: true, claimId: message.author.id })
 
         message.channel.send('You have successfully claimed the ticket!')
-    }
+
+        return;
+
+    } 
 }
 
-// gId chanId catId role msg  | STRINGS
-// so we don't have to see the schema every times --> I change some stuff to make it easier, it's better to not use the _id because using _id can cause errors, very stupid errors
+/*
+const ticket = new mongoose.Schema({
+    gId: { type: String },
+    uId: { type: String },
+    chanId: { type: String },
+    claimed: { type: Boolean },
+    claimId: { type: Boolean }
+  })
+*/
